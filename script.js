21125e0cccda5e247d7059b6f75d23a8f6717847
Dima163163 <khlopovdima@gmail.com>
2023-03-29 18:24:45 +0400

    исправил курс доллра,гривны,юаня

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof true);
 console.log(screens.length);
-console.log(screenPrice + " " + "₽/$/₴/¥");
-console.log(fullPrice + " " + "₽/$/₴/¥");
+console.log(
+  screenPrice + "₽",
+  (screenPrice / 77).toFixed(2) + "$",
+  (screenPrice / 2.09).toFixed(2) + "₴",
+  (screenPrice / 11.18).toFixed(2) + "¥"
+);
+console.log(
+  fullPrice + " " + "₽",
+  (fullPrice / 77).toFixed(2) + "$",
+  (fullPrice / 2.09).toFixed(2) + "₴",
+  (fullPrice / 11.18).toFixed(2) + "¥"
+);
 console.log(screens.toLowerCase().split(","));
 console.log(fullPrice * (rollback / 100));
