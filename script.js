d1199077d9724ad6976c9a1cc1d1e2edfa10f957
Dima163163 <khlopovdima@gmail.com>
2023-04-19 23:44:15 +0400

    исправил параметры функций

--- script.js
+++ script.js
@@ -30,21 +30,21 @@ const getRollbackMessage = function (price) {
   }
 };
 
-const getAllServicePrices = function (servpr1, servpr2) {
-  return servpr1 + servpr2;
+const getAllServicePrices = function (servicepr1, servicepr2) {
+  return servicepr1 + servicepr2;
 };
 
 const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
-function getFullPrice(scrPrc, allSrvPr) {
-  return scrPrc + allSrvPr;
+function getFullPrice(screenPrc, allServicePr) {
+  return screenPrc + allServicePr;
 }
 
 const fullPrice = getFullPrice(screenPrice, allServicePrices);
 
-const getTitle = function (str) {
-  let strNew = str.trim();
-  return strNew[0].toUpperCase() + strNew.slice(1).toLowerCase();
+const getTitle = function (heading) {
+  let headingNew = heading.trim();
+  return headingNew[0].toUpperCase() + headingNew.slice(1).toLowerCase();
 };
 
 const getServicePercentPrices = function (fullPr, rollBk) {
